1ba3b481adfaea27986756d9844f0d46990ca65a
Enrico Soprana <enrico.soprana@studenti.unitn.it>
2017-12-02 16:49:27 +0100

    Aggiunta la possibilità di ottenere le attività associate a un corso

--- src/backend/src/index.js
+++ src/backend/src/index.js
@@ -1,20 +1,62 @@
-const express = require('express');
+"use strict";
+
+const express = require('express'),
+	apicache = require('apicache'),
+	cache = apicache.middleware,
+	bodyParser = require('body-parser'),
+	setTimeoutPromise = require('util').promisify(setTimeout);
+
 const app = express();
-const calendar = require('./sources/calendar.js')
+const calendar = require('./sources/calendar.js');
 
-app.get('/calendar.json',(req,res) => {
-	calendar.getCal(
-		req.query._lang,
-		req.query.anno,
-		req.query.anno2,
-		req.query.codes,
-		req.query.corso,
-		req.query.date
-	).then(generatedJSON => {
-		res.writeHead(200, { 'Content-Type': 'application/json'});
-		res.end(generatedJSON);
-	});
+app.get('/calendar.json',
+	cache('30 seconds',(req,res) => res.statusCode === 200),
+	(req,res) => {
+		calendar.getCal(
+			req.query.lang,
+			req.query.anno,
+			req.query.percorso,
+			req.query.corso,
+			req.query.date
+		).then( generatedJSON => res.json(generatedJSON), reason => {
+			res.status(500);
+			console.log(reason);
+			return res.end('Internal Server Error');
+		});
+	}
+);
 
+app.post('/fromPercorsoToInsegnamenti.json',bodyParser.json({type: 'application/json'}),(req,res) => {
+	function getDDMMYYYYDate() {
+		function pad(s) { return (s < 10) ? '0' + s : s; }
+		var d = new Date();
+		return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('-');
+	}
+
+	return Promise.all(
+		req.body.map( x => calendar.getCal(
+				'it',
+				x.anno,
+				x.percorso,
+				x.corso,
+				getDDMMYYYYDate()
+			).then( data =>
+				({
+					elenco_attivita : data.elenco_attivita,
+					anno: data.annoScolastico,
+					percorso: data.codice_percorso,
+					corso: data.codice_corso,
+					date: data.date
+				})
+			)
+		)
+	).then( cals => res.json(cals));
+});
+
+app.get('/force_cache/*',(req,res) => {
+	const realUrl = req.originalUrl.substring(12,req.originalUrl.length);
+	apicache.clear(realUrl);
+	res.redirect(realUrl);
 });
 
 app.listen(3000);
--- src/backend/src/sources/calendar.js
+++ src/backend/src/sources/calendar.js
@@ -1,66 +1,113 @@
 const fetch = require('node-fetch');
 const FormData = require('form-data');
 
-exports.getCal = (lang,anno,anno2,codes,corso,date) => {
-	if(!Array.isArray(codes))
-		codes=[codes];
+/**
+ * @typedef Evento
+ * @type {object}
+ * @property {{codice_insegnamento: string, docente: string}} insegnamento
+ * @property {{codice_dipartimento: string, codice_aula: string}} luogo
+ * @property {{inizio: string, fine: string}} timestamp
+ * @property {string} tipo
+ *
+ */
 
-	const url = 'https://easyroom.unitn.it/Orario/list_call.php'
+/**
+ * @typedef CalObj
+ * @type {object}
+ * @property {Evento[]} elenco_lezioni
+ * @property {string[]} elenco_insegnamenti
+ * @property {string} annoScolastico
+ * @property {string} codice_percorso
+ * @property {string} codice_corso
+ * @property {string} data Data in formato DD-MM-YYYY
+ */
+
+/**
+ * Restituisce una struttura che indica l'orario secondo i parametri indicati
+ *
+ * @name getCal
+ * @function
+ * @access public
+ * @param lang Lingua richiesta
+ * @param anno Anno scolastico(es: 2017)
+ * @param codice_percorso Percorso seguito(es: P0003|2)
+ * @param codice_corso Corso di laurea seguito(es: 0115G)
+ * @param date Data
+ * @returns {IdsObj} Struttura che indica l'orario secondo i parametri indicati
+ */
+
+exports.getCal = (lang,anno,codice_percorso,codice_corso,date) => {
+	if ((lang === undefined) ||
+		(anno === undefined) ||
+		(codice_percorso === undefined) ||
+		(codice_corso === undefined) ||
+		(date === undefined)
+	) {
+		return Promise.reject(
+			new Error('Paramentri mancanti nella richiesta.\n' +
+				' I parametri necessari sono _lang,anno,codice_percorso,codice_corso,date'
+		));
+	}
+
+	const url = 'https://easyroom.unitn.it/Orario/grid_call.php';
 	const form = new FormData();
-	form.append('_lang','it');
+	form.append('_lang',lang);
 	form.append('anno',anno);
-	form.append('anno2',anno2);
-	form.append('ar_codes_',codes.join('|'));
-	form.append('ar_select_',(new Array(codes.length)).fill(true).join('|'));
-	form.append('corso',corso);
-	form.append('corso',corso);
+	form.append('anno2',codice_percorso);
+	form.append('corso',codice_corso);
 	form.append('date',date);
+	form.append('all_events',1);
 	form.append('form-type','corso');
 
 	return fetch(url,{
 			method: 'POST',
 			body: form
 		})
-		.then(data => data.text())
-		.then(text => {
-			const json = JSON.parse(text);
-			const elenco_lez = [];
+		.then(data => data.json())
+		.then(json => {
+			try{
+				const elenco_lez = json.celle.map(cella => {
+					const dataSplit = cella.data.split('-');
+					const orarioInizioSplit = cella.ora_inizio.split(':');
+					const orarioFineSplit = cella.ora_fine.split(':');
+					const begTimestamp = new Date(
+						dataSplit[2],dataSplit[1],dataSplit[0],
+						orarioInizioSplit[0],orarioInizioSplit[1]
+					);
+					const endTimestamp = new Date(
+						dataSplit[2],dataSplit[1],dataSplit[0],
+						orarioFineSplit[0],orarioFineSplit[1]
+					);
+					const codiceAulaSplit = cella.codice_aula.split('/');
 
-			for(let i=0;i<json.contains_data;i++) {
-				const dataSplit = json[i].data.split('-');
-				const orarioInizioSplit = json[i].ora_inizio.split(':');
-				const orarioFineSplit = json[i].ora_fine.split(':');
-				const begTimestamp = new Date(
-					dataSplit[2],dataSplit[1],dataSplit[0],
-					orarioInizioSplit[0],orarioInizioSplit[1]
-				);
-				const endTimestamp = new Date(
-					dataSplit[2],dataSplit[1],dataSplit[0],
-					orarioFineSplit[0],orarioFineSplit[1]
-				);
-				const codiceAulaSplit = json[i].codice_aula.split('/');
-
-				elenco_lez.push({
-					insegnamento: {
-						codice_insegnamento: json[i].codice_insegnamento,
-//						nome_insegnamento: json[i].nome_insegnamento,
-						docente: json[i].docente
-					},
-					luogo:{
-						codice_dipartimento: codiceAulaSplit[0],
-						codice_aula: codiceAulaSplit[1],
-					},
-					timestamp: {
-						inizio: begTimestamp,
-						fine: endTimestamp,
-					},
-					tipo: json[i].tipo
+					return {
+						attivita: {
+							codice_attivita: cella.codice_insegnamento,
+							docente: cella.docente
+						},
+						luogo:{
+							codice_dipartimento: codiceAulaSplit[0],
+							codice_aula: codiceAulaSplit[1],
+						},
+						timestamp: {
+							inizio: begTimestamp,
+							fine: endTimestamp,
+						},
+						tipo: cella.tipo
+					}
 				});
-			}
 
-			return JSON.stringify({
-				elenco_lezioni: elenco_lez
-			});
+				return {
+					elenco_lezioni: elenco_lez,
+					elenco_attivita: json.legenda.map(corso => corso.codice),
+					annoScolastico: anno,
+					codice_percorso: codice_percorso,
+					codice_corso: codice_corso,
+					date: date
+				};
+			} catch(err){
+				return Promise.reject(new Error(err));
+			}
 		});
 }
 
