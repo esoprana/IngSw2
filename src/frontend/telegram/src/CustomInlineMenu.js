32a7ca0bf75daf5e1dbae9b26d5d281e4e50acf8
Unknown <jan.oradini@studenti.unitn.it>
2017-12-13 07:54:42 +0000

    more or less
    c'è anora tanto da sistemare, i'm sorry

--- src/frontend/telegram/src/CustomInlineMenu.js
+++ src/frontend/telegram/src/CustomInlineMenu.js
@@ -1,6 +1,8 @@
 'use strict';
 var scope;
 var JsonData = require('./codes.json') //poi da togliere
+const Calendar = require('../../../backend/src/sources/calendar.js')
+
 
 class CustomInlineMenu {
 
@@ -11,26 +13,27 @@ class CustomInlineMenu {
 
     getJsonMenu() {
 
-        var conteggioCorsi = 0;
+
         var datiCorsi = [];
         var tmp = [];
+        var codici = []
         for (var anno in JsonData.corsi) {
             for (var corso in JsonData.corsi[anno].elenco) {
-                conteggioCorsi++;
-                console.log(JsonData.corsi[anno].elenco[corso].label);
+                var codiciCorso = []
+                codiciCorso.push(anno)
+                codiciCorso.push(corso);
                 tmp.push(JsonData.corsi[anno].elenco[corso].label);
                 for (var annoCorso in JsonData.corsi[anno].elenco[corso].elenco_anni) {
                     var annoTmp = JsonData.corsi[anno].elenco[corso].elenco_anni[annoCorso][0].label;
-                    console.log(annoTmp);
                     tmp.push(annoTmp);
+                    codiciCorso.push(annoCorso)
+
                 }
 
-
-        
-               console.log("DIOCANE :" + tmp + "\n")
+                codici.push(codiciCorso)
                 datiCorsi.push(tmp)
-                tmp = [];
 
+                tmp = [];
 
             }
 
@@ -46,21 +49,121 @@ class CustomInlineMenu {
         jsonMenu[key] = [];
 
         for (var index = 0; index < datiCorsi.length; index++) {
-            console.log("A" + datiCorsi[index]);
-            const m = [];
-            for (var anno = 0; anno < datiCorsi[index].length; ++anno) {
-                console.log("***" + index)
+            var m = [];
+            var codice_anno = codici[index][0]
+            var codice_corso = codici[index][1]
+
+            for (var anno = 1; anno < datiCorsi[index].length; anno++) {
+                var codice_percorso = codici[index][anno + 1]
+                var ghesboro = this;
+                var calendarJson
+                var jsonData = JsonData
+
+
                 m.push(
                     {
                         text: "" + datiCorsi[index][anno],
-                        callback: () => { scope.sendMessage("Eseguire chhiamata per orario con parametri " + "Corso:  " + datiCorsi[index][0] + "Anno: " + datiCorsi[index][anno]) }
+                        callback: (req, res) => {
+
+                            calendarJson = Calendar.getCal(
+                                'it', codice_anno, codice_percorso, codice_corso, new Date().toISOString())
+                                .then(function (result) {
+                                    var i = 0
+                                    var jsonCalendar = JSON.parse(JSON.stringify(result))
+                                    var orario = []
+
+                                    for (var i = 0; i < result.elenco_lezioni.length; i++) {
+                                        var tmp = []
+
+                                        var codiceCorso = jsonCalendar.elenco_lezioni[i].attivita.codice_attivita
+                                        //boh ci sono altre aule misteriose....
+                                        //console.log(jsonCalendar.elenco_lezioni[i].luogo.codice_aula)
+                                        var nomeCorso = jsonData.attivita[codice_anno].elenco[codiceCorso].label
+
+                                        //i corsi non sono ordinati per data
+                                        //devo prenderli tutti dividerli per giorno per ora poi associarli al label del corso e formattare il testo 
+                                        //poi già che son divisi e data la data ci sarebbbe da prendere da la settimana in corso o dall inizio settimana 
+                                        //fatto ciò c'è da ristrutturare tutto il codice e fare le varie associazioni
+
+                                        //mettere anno solo se diverso dal corrente?
+                                        var dataInizio = jsonCalendar.elenco_lezioni[i].timestamp.inizio
+                                        var dataFine = jsonCalendar.elenco_lezioni[i].timestamp.fine
+                                        var anno = dataInizio.substring(0, 4)
+                                        var mese = dataInizio.substring(5, 7)
+                                        var giorno = dataInizio.substring(8, 10)
+                                        var oraInizio = dataInizio.substring(11, 16)
+                                        var oraFine = dataFine.substring(11, 16)
+                                        var dataFormattata = oraInizio + "-" + oraFine + "\t" + giorno + "/" + mese
+                                        tmp.push(nomeCorso)
+                                        tmp.push(dataInizio)
+                                        tmp.push(dataFormattata)
+
+                                        orario.push(tmp)
+                                        tmp = []
+
+                                        i++
+                                    }
+                                    orario.sort(function (a, b) {
+
+                                        if (a[1] > b[1]) {
+                                            return 1
+                                        }
+                                        if (a[1] < b[1]) {
+                                            return -1
+                                        }
+                                        if (a[1] === b[1]) {
+                                            if ((Number.parseInt(a[2].substring(0, 2))) > (Number.parseInt(a[2].substring(0, 2)))) {
+                                                return 1
+                                            }
+                                            if ((Number.parseInt(a[2].substring(0, 2))) < (Number.parseInt(a[2].substring(0, 2)))) {
+                                                return -1
+                                            }
+                                            if ((Number.parseInt(a[2].substring(0, 2))) === (Number.parseInt(a[2].substring(0, 2)))) {
+                                                return 0
+                                            }
+
+
+                                        }
+
+                                    })
+                                    //console.log(orario)
+                                    var giorno_lezione="" 
+                                    var messaggio=""
+                                
+                                    for(var i=0; i< orario.length; i++){
+                                        console.log(orario[i][2])
+
+                                        if(orario[i][2].substring(13)!==giorno_lezione && messaggio!==""){
+                                            //mettere uno sleep
+                                            scope.sendMessage(messaggio)
+                                            giorno_lezione=orario[i][2].substring(13)
+                                            messaggio+=orario[i][2]+ "\n"+ orario[i][0]+"\n\n"
+
+                                            
+                                        }
+                                        else{messaggio+=orario[i][2]+ "\n"+ orario[i][0]+"\n\n"}
+
+                                    }
+
+
+
+
+                                    
+                                    return
+                                });
+
+
+
+
+                            // 
+
+                        }
 
                     }
                 );
+
             }
 
-
-            //            if (datiCorsi[index][0]) {
             jsonMenu[key].push(
                 {
                     text: "" + datiCorsi[index][0],
@@ -70,32 +173,20 @@ class CustomInlineMenu {
                 }
 
             )
-            //            }
         }
-        //perché cazzo salta un ciclo ghesboro
-        for (var index = 0; index < datiCorsi.length; index++) {
-            console.log("---" + index)
-            /*
-                        for (var anno = 0; anno < datiCorsi[index].length; ++anno) {
-                            console.log("***"+index)
-                            if (jsonMenu[key][index] !== undefined && jsonMenu[key][index].menu !== undefined) {
-                                
-                                jsonMenu[key][index].menu.push(
-                                    {
-                                        text: "" + datiCorsi[index][anno],
-                                        callback: () => { scope.sendMessage("Eseguire chhiamata per orario con parametri "+"Corso:  "+datiCorsi[index][0]+"Anno: " + datiCorsi[index][anno]) }
-            
-                                    }
-                                );
-                
-                            }
-                            
-                        }*/
-            console.log(index)
-        }
-        //console.log(JSON.stringify(jsonMenu))
         return jsonMenu
     }
 }
 
 module.exports = { CustomInlineMenu }
+
+/*
+
+    
+    -chiedere per sessioni eventi e orario
+    -chiedere giorno settimana mese
+    -chiedere corso, etc
+    - Salvare dati utenti
+
+
+*/
